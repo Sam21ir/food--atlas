6c1c928312a6b82c1387d43b4f75cf095c5089dd
alphabdo <abdokhatib566@gmail.com>
2025-11-27 11:34:08 +0100

    home page is done

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/" className="logo-text">FOOD ATLAS</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/recettes" className="nav-link">RECETTES</Link>
+        <Link to="/contact" className="nav-link">CONTACT</Link>
+        <Link to="/admin/ajouter-recette" className="nav-link add-recipe-btn">
+          AJOUTER RECETTE
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
